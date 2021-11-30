890d72a86654c8c5c9a278b583adc09e820a63f0
Md Shahrin Bin Selim <mdshahrinbinselim1991@gmail.com>
2021-11-30 21:56:14 +0600

    dffd
    dfdf

---
+++ review.js
@@ -0,0 +1,17 @@
+[
+    {
+        "img":"https://media.istockphoto.com/photos/learn-to-love-yourself-first-picture-id1291208214?b=1&k=20&m=1291208214&s=170667a&w=0&h=sAq9SonSuefj3d4WKy4KzJvUiLERXge9VgZO-oqKUOo=",
+        "name":"Robina Frilink",
+        "description":" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam architecto atque sapiente quidem enim dignissimos voluptatibus accusamus aperiam obcaecati, reiciendis incidunt iusto? Reiciendis illum magni soluta eos delectus nulla illo!"
+    },
+    {
+        "img":"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aHVtYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        "name":"Daniel dani",
+        "description":" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam architecto atque sapiente quidem enim dignissimos voluptatibus accusamus aperiam obcaecati, reiciendis incidunt iusto? Reiciendis illum magni soluta eos delectus nulla illo!"
+    },
+    {
+        "img":"https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGh1bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "name":"Hiris Harmand",
+        "description":" Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam architecto atque sapiente quidem enim dignissimos voluptatibus accusamus aperiam obcaecati, reiciendis incidunt iusto? Reiciendis illum magni soluta eos delectus nulla illo!"
+    },
+]
