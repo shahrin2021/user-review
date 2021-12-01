8e7d0fab899cb52d2c3c57e033c66ce578665cb8
Md Shahrin Bin Selim <mdshahrinbinselim1991@gmail.com>
2021-12-01 12:19:02 +0600

    project
    project

---
+++ project.js
@@ -0,0 +1,39 @@
+
+[
+    {
+        "id":"1",
+        "name":"Parsonal Protfolio",
+        "img":"https://i.ibb.co/hczPc3p/prot.png",
+        "description":"This  website is a professional full-stack web-developer Protfolio. this website reveal professional image and exprience of a webdeviloper .   "
+    },
+    {
+        "id":"2",
+        "name":"Shoes sales html template",
+        "img":"https://i.ibb.co/vvJKTBj/html.png",
+        "description":"This website is an ecommerce html template , Through this website visitor can buy shoes ."
+    },
+    {
+        "id":"3",
+        "name":"E-commerce-javascript project",
+        "img":"https://i.ibb.co/gZSLPJG/shope.png",
+        "description":"This is a e-commerce website . On the Home page have some  product and live calculation using js  "
+    }
+    // {
+    //     "id":"4",
+    //     "name":"Car Mechanic website",
+    //     "img":"https://i.ibb.co/mCqPY8Z/mechanic.png",
+    //     "description":""
+    // },
+    // {
+    //     "id":"5",
+    //     "name":"Car-Booking Full-stack project",
+    //     "img":"https://i.ibb.co/6ZZJvBZ/car.png",
+    //     "description":""
+    // },
+    // {
+    //     "id":"6",
+    //     "name":"Donte for climate-changing",
+    //     "img":"https://i.ibb.co/1dnGSt8/donar.png",
+    //     "description":""
+    // }
+]
