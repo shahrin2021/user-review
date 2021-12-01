9d0b49799821b4313532ac3606e40ea3cfff73f8
Md Shahrin Bin Selim <mdshahrinbinselim1991@gmail.com>
2021-12-01 13:43:19 +0600

    k
    k

--- project.js
+++ project.js
@@ -1,19 +1,19 @@
 
 [
     {
-        "id":"1",
+        "id":1,
         "name":"Parsonal Protfolio",
         "img":"https://i.ibb.co/hczPc3p/prot.png",
         "description":"This  website is a professional full-stack web-developer Protfolio. this website reveal professional image and exprience of a webdeviloper .   "
     },
     {
-        "id":"2",
+        "id":2,
         "name":"Shoes sales html template",
         "img":"https://i.ibb.co/vvJKTBj/html.png",
         "description":"This website is an ecommerce html template , Through this website visitor can buy shoes ."
     },
     {
-        "id":"3",
+        "id":3,
         "name":"E-commerce-javascript project",
         "img":"https://i.ibb.co/gZSLPJG/shope.png",
         "description":"This is a e-commerce website . On the Home page have some  product and live calculation using js  "
